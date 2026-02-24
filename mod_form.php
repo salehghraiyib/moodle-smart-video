6e20f9856bac54916a13aa5ec5da1a8e6b1d910e
salehghraiyib <saleh.ghrayeb@gmail.com>
2026-02-24 09:04:09 +0100

    Updated text strings

--- mod_form.php
+++ mod_form.php
@@ -7,8 +7,6 @@ class mod_smartvideo_mod_form extends moodleform_mod {
         global $CFG;
         $mform = $this->_form;
 
-        // 1. Standard Name Field
-// 1. Standard Name Field
         $mform->addElement('header', 'general', get_string('general', 'form'));
         $mform->addElement('text', 'name', get_string('name', 'smartvideo'), array('size'=>'48'));
         $mform->addRule('name', null, 'required', null, 'client');
@@ -16,8 +14,8 @@ class mod_smartvideo_mod_form extends moodleform_mod {
 
         $this->standard_intro_elements();
 
-        // 2. The Video Upload Manager
-        // This creates the drag-and-drop box
+
+        //video
         $mform->addElement('filemanager', 'videofile', get_string('videofile', 'smartvideo'), null,
             array('subdirs' => 0, 'maxbytes' => $CFG->maxbytes, 'maxfiles' => 1, 'accepted_types' => array('.mp4', '.mov', '.webm')));
 
@@ -29,14 +27,13 @@ class mod_smartvideo_mod_form extends moodleform_mod {
             array(
                 'subdirs' => 0, 
                 'maxbytes' => $CFG->maxbytes, 
-                'maxfiles' => 1, // Limit to 1 PDF
-                'accepted_types' => array('.pdf') // Restrict to PDF only
+                'maxfiles' => 1,
+                'accepted_types' => array('.pdf')
             )
         );
         
         $mform->addHelpButton('slidefile', 'slidefile', 'smartvideo');
 
-        // Standard Course module elements
         $this->standard_coursemodule_elements();
         $this->add_action_buttons();
     }
--- settings.php
+++ settings.php
@@ -2,9 +2,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 if ($ADMIN->fulltree) {
-
-    // 1. Create a new Settings Page
-    // We manually create the object to ensure it is the correct type (admin_settingpage)
     $settings_page = new admin_settingpage(
         'mod_smartvideo',                
         get_string('pluginname', 'mod_smartvideo')
@@ -25,10 +22,8 @@ if ($ADMIN->fulltree) {
     ));
 
     if (isset($settings) && $settings instanceof admin_category) {
-        // If Moodle gave us a category, we add our page to it using a string name first
         $settings->add('mod_smartvideo', $settings_page);
     } else {
-        // Fallback: Add directly to the 'modsettings' category in the global ADMIN tree
         $ADMIN->add('modsettings', $settings_page);
     }
 }
